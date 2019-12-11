8d7635f3508dff1c251c22b2bdc9bf97d22165c0
liggger <zhichengzhou_96@163.com>
2019-12-11 16:54:53 +0000

    add marker

--- MoveNow/app/src/main/java/uk/ac/shef/oak/com4510/model/Image.java
+++ MoveNow/app/src/main/java/uk/ac/shef/oak/com4510/model/Image.java
@@ -4,14 +4,32 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import com.google.android.gms.maps.model.LatLng;
+
 @Entity
 public class Image {
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "image_id")
     private int image_id;
 
+//    @ColumnInfo(name = "path_id")
+//    private int path_id;
+//
+//    @ColumnInfo(name = "latitude")
+//    private double latitude;
+//
+//    @ColumnInfo(name = "longitude")
+//    private double longitude;
+
     @ColumnInfo(typeAffinity = ColumnInfo.BLOB)
     private byte[] picture;
+
+//    public Image(int path_id, double latitude, double longitude, byte[] picture) {
+//        this.path_id = path_id;
+//        this.latitude = latitude;
+//        this.longitude = longitude;
+//        this.picture = picture;
+//    }
 
     public Image(byte[] picture) {
         this.picture = picture;
@@ -25,6 +43,14 @@ public class Image {
         this.image_id = image_id;
     }
 
+//    public int getPath_id() {
+//        return path_id;
+//    }
+//
+//    public void setPath_id(int path_id) {
+//        this.path_id = path_id;
+//    }
+
     public byte[] getPicture() {
         return picture;
     }
@@ -32,4 +58,20 @@ public class Image {
     public void setPicture(byte[] picture) {
         this.picture = picture;
     }
+
+//    public double getLatitude() {
+//        return latitude;
+//    }
+//
+//    public void setLatitude(double latitude) {
+//        this.latitude = latitude;
+//    }
+//
+//    public double getLongitude() {
+//        return longitude;
+//    }
+//
+//    public void setLongitude(double longitude) {
+//        this.longitude = longitude;
+//    }
 }
--- MoveNow/app/src/main/java/uk/ac/shef/oak/com4510/views/Home/HomeStopFragment.java
+++ MoveNow/app/src/main/java/uk/ac/shef/oak/com4510/views/Home/HomeStopFragment.java
@@ -1,13 +1,18 @@
-package uk.ac.shef.oak.com4510.views;
+package uk.ac.shef.oak.com4510.views.Home;
 
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.Matrix;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,29 +22,35 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import pl.aprilapps.easyphotopicker.DefaultCallback;
-import uk.ac.shef.oak.com4510.R;
-
 import pl.aprilapps.easyphotopicker.EasyImage;
+import uk.ac.shef.oak.com4510.R;
 import uk.ac.shef.oak.com4510.model.Image;
 import uk.ac.shef.oak.com4510.viewmodels.ImageViewModel;
 
@@ -55,6 +66,10 @@ public class HomeStopFragment extends Fragment implements OnMapReadyCallback {
     private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
     private Activity activity;
     private GoogleMap mMap;
+    private Location mCurrentLocation;
+    private String mLastUpdateTime;
+    private ArrayList<LatLng> route = new ArrayList<>();
+    private Polyline polyline;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -69,16 +84,14 @@ public class HomeStopFragment extends Fragment implements OnMapReadyCallback {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map);
-//        SupportMapFragment mapFragment = getFragmentManager().findFragmentById(R.id.map);
-//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-//                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment)this.getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
         imageViewModel = ViewModelProviders.of(this).get(ImageViewModel.class);
 
         if(allPermissionsGranted()){
             initEasyImage(); //start camera if permission has been granted by user
+            startLocationUpdates();
             System.out.println("yes");
         } else{
             ActivityCompat.requestPermissions(getActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
@@ -88,6 +101,8 @@ public class HomeStopFragment extends Fragment implements OnMapReadyCallback {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mMap.addMarker(new MarkerOptions().position(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()))
+                        .title(mLastUpdateTime));
                 EasyImage.openCamera(getActivity(), 0);
             }
         });
@@ -96,8 +111,9 @@ public class HomeStopFragment extends Fragment implements OnMapReadyCallback {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                NavController controller = Navigation.findNavController(v);
-                controller.navigate(R.id.action_homeStopFragment_to_navigation_home);
+                stopLocationUpdates();
+//                NavController controller = Navigation.findNavController(v);
+//                controller.navigate(R.id.action_homeStopFragment_to_navigation_home);
             }
         });
     }
@@ -111,6 +127,73 @@ public class HomeStopFragment extends Fragment implements OnMapReadyCallback {
         LatLng sydney = new LatLng(53.162080, -1.480865);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            } else {
+
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        ACCESS_FINE_LOCATION);
+            }
+
+            return;
+        }
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(500);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null /* Looper */);
+    }
+
+    private void stopLocationUpdates(){
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    }
+
+    LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+            mCurrentLocation = locationResult.getLastLocation();
+            mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+            Log.i("MAP", "new location " + mCurrentLocation.toString());
+            if (mMap != null){
+                route.add(new LatLng(mCurrentLocation.getLatitude(),mCurrentLocation.getLongitude()));
+            }
+            polyline = mMap.addPolyline(new PolylineOptions().addAll(route).width(10).color(Color.BLUE));
+
+//            mMap.addMarker(new MarkerOptions().position(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()))
+//                        .title(mLastUpdateTime));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()), 14.0f));
+        }
+    };
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                            mLocationCallback, null /* Looper */);
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+        }
     }
 
     private void initEasyImage() {
@@ -134,7 +217,6 @@ public class HomeStopFragment extends Fragment implements OnMapReadyCallback {
 
             @Override
             public void onImagesPicked(List<File> imageFiles, EasyImage.ImageSource source, int type) {
-                System.out.println(imageFiles.get(0).getAbsolutePath());
                 Bitmap bitmap= BitmapFactory.decodeFile(imageFiles.get(0).getAbsolutePath());
                 Bitmap scaleBitmap = scaleBitmap(bitmap, 0.25f);
                 byte[] picture = getBitmapAsByteArray(scaleBitmap);
